a62dc134faf9667202656febc73420dc6e58e989
PWman <36227824+PWman@users.noreply.github.com>
2020-03-15 16:57:03 +0000

    Add files via upload

--- EXPERIMENT_1.py
+++ EXPERIMENT_1.py
@@ -12,8 +12,14 @@ def initialise_networks():
     alex.classifier[-1] = nn.Linear(4096, 2)
     vgg = models.vgg16(pretrained=False)
     vgg.classifier[-1] = nn.Linear(4096, 2)
+
+    vgg_s = models.vgg11(pretrained=False)
+    vgg_s.classifier[-1] = nn.Linear(4096, 2)
+
     resnet = models.resnet50(pretrained=False)
     resnet.fc = nn.Linear(2048, 2)
+    resnet_s = models.resnet18(pretrained=False)
+    resnet_s.fc = nn.Linear(512, 2)
 
     incept = models.googlenet(pretrained=False)
     incept.aux1.fc2 = nn.Linear(1024, 2)
@@ -27,6 +33,7 @@ def initialise_networks():
     for param in alex_pt.classifier.parameters():
         param.requires_grad = True
 
+
     vgg_pt = models.vgg16(pretrained=True)
     vgg_pt.classifier[-1] = nn.Linear(4096, 2)
     for param in vgg_pt.features.parameters():
@@ -34,10 +41,24 @@ def initialise_networks():
     for param in vgg_pt.classifier.parameters():
         param.requires_grad = True
 
+
+    vgg_pt_s = models.vgg11(pretrained=True)
+    vgg_pt_s.classifier[-1] = nn.Linear(4096, 2)
+    for param in vgg_pt_s.features.parameters():
+        param.requires_grad = False
+    for param in vgg_pt_s.classifier.parameters():
+        param.requires_grad = True
+
     resnet_pt = models.resnet50(pretrained=True)
     for param in resnet_pt.parameters():
         param.requires_grad = False
     resnet_pt.fc = nn.Linear(2048,2)
+
+    resnet_pt_s = models.resnet18(pretrained=True)
+    for param in resnet_pt_s.parameters():
+        param.requires_grad = False
+    resnet_pt_s.fc = nn.Linear(512,2)
+
 
     incept_pt = models.googlenet(pretrained=True)
     for param in incept_pt.parameters():
@@ -46,13 +67,17 @@ def initialise_networks():
 
     nets = [
         {"name": config.net_names[0], "net": alex, "train": alex},
-        {"name": config.net_names[1], "net": vgg, "train": vgg},
-        {"name": config.net_names[2], "net": resnet, "train": resnet},
-        {"name": config.net_names[3], "net": incept, "train": incept},
-        {"name": config.net_names[4], "net": alex_pt, "train": alex_pt.classifier},
-        {"name": config.net_names[5], "net": vgg_pt, "train": vgg_pt.classifier},
-        {"name": config.net_names[6], "net": resnet_pt, "train": resnet_pt.fc},
-        {"name": config.net_names[7], "net": incept_pt, "train": incept_pt.fc}
+        {"name": config.net_names[1], "net": vgg_s, "train": vgg_s},
+        {"name": config.net_names[2], "net": vgg, "train": vgg},
+        {"name": config.net_names[3], "net": resnet_s, "train": resnet_s},
+        {"name": config.net_names[4], "net": resnet, "train": resnet},
+        {"name": config.net_names[5], "net": incept, "train": incept},
+        {"name": config.net_names[6], "net": alex_pt, "train": alex_pt.classifier},
+        {"name": config.net_names[7], "net": vgg_pt_s, "train": vgg_pt_s.classifier},
+        {"name": config.net_names[8], "net": vgg_pt, "train": vgg_pt.classifier},
+        {"name": config.net_names[9], "net": resnet_pt_s, "train": resnet_pt_s.fc},
+        {"name": config.net_names[10], "net": resnet_pt, "train": resnet_pt.fc},
+        {"name": config.net_names[11], "net": incept_pt, "train": incept_pt.fc}
     ]
     return nets
 
